0ef249cc3e00aa4ebbca9a8c1d754600e5232a8f
Łukasz Lityński <hex13code@gmail.com>
2015-01-30 00:34:46 +0100

    simplify lupa.js code by using _.merge and _.indexBy

--- src/lupa.js
+++ src/lupa.js
@@ -1,8 +1,7 @@
 var glob = require('glob');
 var fs = require('fs');
+
 var _ = require('lodash');
-
-
 
 
 var lupa = module.exports = {
@@ -17,26 +16,15 @@ var lupa = module.exports = {
         function getDataFromPlugins(plugins) {
             return plugins.map(function (plugin) {
                 var res = plugin.readFiles(fs, files);
-                console.log("Data from plugin ",plugin.name, ": ", JSON.stringify(res, null, 2), "\n");
+                console.log("DEBUG Data from plugin ",plugin.name, ": ", JSON.stringify(res, null, 2), "\n");
                 return res;
             });
         }
 
-        function mergeDataFromPlugins(dataFromPlugins) {
-            return dataFromPlugins.reduce(function (result, dataFromPlugin) {
-                return dataFromPlugin.reduce(function (result, fileData) {
-                    var dict = result[fileData.file] || (result[fileData.file] = {});
-                    _.assign(dict, fileData);
-                    return result;
-                }, result);
-            }, {});
-        }
-
-        return mergeDataFromPlugins(getDataFromPlugins(plugins));
+        return getDataFromPlugins(plugins).map(function (filesMetadata) {
+            return _.indexBy(filesMetadata, 'file');
+        }).reduce(function (dict, chunk) {
+            return _.merge(dict, chunk);
+        }, {});
     }
-
-}
-
-
-if (require.main === module) (function startFromCLI() {
-})();
+};
