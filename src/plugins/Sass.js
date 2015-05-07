6abafe7b86ab56e44567042b8528478dfb2d8746
Łukasz Lityński <hex13code@gmail.com>
2015-05-07 12:03:09 +0200

    refactor Sass plugin

--- src/plugins/Sass.js
+++ src/plugins/Sass.js
@@ -1,48 +1,70 @@
 var _ = require('lodash');
 
-module.exports = function () {
-    return function (code) {
-        code = code.replace(/\/\/.*/g,'');
+function stripComments(code) {
+    return code.replace(/\/\/.*/g,'');
+}
 
+function parseMixins(code) {
+    var re, match;
+
+    var mixins = {
+        declarations: [],
+        uses: [],
+    };
+
+    re = /@mixin +([a-zA-z0-9-]+)/g;
+    while (match = re.exec(code)) {
+        mixins.declarations.push(match[1]);
+    }
+
+    re = /(?:\n|^)\s*(?:@include *|\+)([a-zA-z0-9-]+)/g;
+    while (match = re.exec(code)) {
+        mixins.uses.push(match[1]);
+    }
+
+    mixins.uses = _.uniq(mixins.uses);
+    return mixins;
+}
+
+function parseVariables (code) {
+    var re, match;
+
+    var variables = [];
+    re = /(\$[\w-]+)/g;
+    while (match = re.exec(code)) {
+        variables.push(match[1]);
+    }
+
+    return _.uniq(variables);
+}
+
+
+function parseClasses(code) {
+    var re, match;
+
+    var classes = [];
+    re = /(\.[a-zA-Z][\w-]*)/g;
+    while (match = re.exec(code)) {
+        classes.push(match[1]);
+    }
+    return _.uniq(classes);
+}
+
+module.exports = function () {
+
+    return function (code) {
+        code = stripComments(code);
         var codeWithoutParentheses = code.replace(/\(.*?\)/g, '');
 
-        var variables = [];
-        var classes = [];
-        var mixins = {
-            declarations: [],
-        };
+        var mixins = parseMixins(code);
+        var variables = parseVariables(code);
+        var classes = parseClasses(codeWithoutParentheses);
 
-        var uses = [];
-
-        var re, match;
-
-        re = /@mixin +([a-zA-z0-9-]+)/g;
-        while (match = re.exec(code)) {
-            mixins.declarations.push(match[1]);
-        }
-
-        re = /(?:\n|^)\s*(?:@include *|\+)([a-zA-z0-9-]+)/g;
-        while (match = re.exec(code)) {
-            uses.push(match[1]);
-        }
-
-        mixins.uses = _.uniq(uses);
-
-
-        re = /(\$[\w-]+)/g;
-        while (match = re.exec(code)) {
-            variables.push(match[1]);
-        }
-
-        re = /(\.[a-zA-Z][\w-]*)/g;
-        while (match = re.exec(codeWithoutParentheses)) {
-            classes.push(match[1]);
-        }
 
         var data = {
             mixins: mixins,
-            variables: _.uniq(variables),
-            classes: _.uniq(classes)
+            variables: variables,
+            classes: classes
 
         };
         return data;
