45ff9fdd70d457523ad3b3c2d9ae6a9cf65ebdf9
Łukasz Lityński <hex13code@gmail.com>
2015-10-28 03:27:02 +0100

    support (partially) CommonJS modules in ES6ModuleInfo plugin

--- src/plugins/ES6ModuleInfo.js
+++ src/plugins/ES6ModuleInfo.js
@@ -4,14 +4,33 @@ module.exports = function () {
     return function (code, filename) {
         var ast = esprima.parse(code, {sourceType: 'module'});
         var body = ast.body;
-        var imports = body.filter(function (node) {
+        var es6imports = body.filter(function (node) {
             return node.type == 'ImportDeclaration';
         }).map(function (node) {
             return node.source.value;
         });
+        var requires = body.filter(function (node) {
+            // TODO: this doesn't detect something like this:
+            // var something = require('module')(somearguments);
+            // only this:
+            // var something = require('module');
+            if (node.type !== 'VariableDeclaration')
+                return false;
+            var init = node.declarations[0].init;
+            if (init.type != 'CallExpression')
+                return false;
+            if (init.callee.name != 'require')
+                return false;
+            return true;
+
+        }).map(function (node) {
+            return node.declarations[0].init.arguments[0].value;
+        });
         return {
             name: filename,
-            imports: imports
+            es6imports: es6imports,
+            requires: requires,
+            imports: es6imports.concat(requires)
         };
     };
 };
