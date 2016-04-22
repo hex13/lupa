df62a566b8272dde3aa17448815ec534813b8190
Łukasz Lityński <hex13code@gmail.com>
2016-04-22 13:47:09 +0200

    add mock

---
+++ plugins/mocks/imports.js
@@ -0,0 +1,11 @@
+// var commented = require('not')
+var fs = require('fs');
+let File = require('vinyl');
+const Rx = require('Rx');
+import React from 'react';
+
+import {Cat, Dog} from 'animals';
+
+// TODO support this kind of import ( require('module').variableName  )
+// it's currently not supported
+var extname = require('path').extname;
