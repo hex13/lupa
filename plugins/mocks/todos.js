f1976504c7eddb128dcf62383b0c02fcb1723fc6
Łukasz Lityński <hex13code@gmail.com>
2016-04-23 21:05:28 +0200

    todo plugin

---
+++ plugins/mocks/todos.js
@@ -0,0 +1,16 @@
+//TODO import react
+
+// TODO: change name to something better
+function foo() {
+
+}
+
+class Abc extends React.Component () {
+// TODO insert render function
+}
+
+
+// it shouldn't be be parsed:
+function renderIntoDocument() {
+
+}
---
+++ plugins/todos.js
@@ -0,0 +1,31 @@
+var Rx = require('rx');
+var Metadata = require('../src/metadata');
+module.exports = function getTodos(file) {
+    return Rx.Observable.create(
+        observer => {
+            const regexp = /\Wtodo\W+(\w.*)/i;
+            const code = file.contents.toString();
+            const lines = code.split('\n');
+            const result = [];
+            lines.forEach(function (line, i) {
+                const match = line.match(regexp);
+                if (match) {
+                    result.push({
+                        type: 'todo',
+                        name: match[1],
+                        loc: {
+                            start: {
+                                column: 0, line: i
+                            },
+                            end: {
+                                column: 0, line: i + 1
+                            }
+                        }
+                    })
+                }
+            });
+            var clone = Metadata.addMetadata(file, result);
+            observer.onNext(clone);
+        }
+    );
+}
