fc6a68bae75d568df942a66b0778287080a5519d
Łukasz Lityński <hex13code@gmail.com>
2016-05-06 20:04:44 +0200

    parserOptions

---
+++ parsers/parserOptions.js
@@ -0,0 +1,12 @@
+module.exports = {
+    '.js': {
+       sourceType: 'module',
+       ecmaVersion: 7,
+       locations: true,
+       plugins: {
+           jsx: true
+       },
+       allowHashBang: true,
+       loc: true
+    }
+}
