04f9080cf3ef41ca231d25aa8efaef71e69603d8
Łukasz Lityński <hex13code@gmail.com>
2016-05-08 03:19:37 +0200

    fix

--- plugins/coffeescript.js
+++ plugins/coffeescript.js
@@ -49,7 +49,7 @@ module.exports = function coffee (file) {
                     functions.push({
                         type: 'function',
                         name: match[1] || '',
-                        params: '???',                        
+                        params: [],                        
                         loc: {
                             start: {column: 0, line: i +1},
                             end: {column: 0, line: i + 2}
